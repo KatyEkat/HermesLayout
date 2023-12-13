2693b8f9d660daf7397e4efccc29bb4d73e2c698
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-12-13 17:53:30 +0300

    added logical for opening popups services page

--- scripts/services.js
+++ scripts/services.js
@@ -1,12 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelectorAll(".services_btn");
-    const popup = document.querySelectorAll(".popup");
+document.addEventListener('DOMContentLoaded', function () {
+    const serviceButtons = document.querySelectorAll('.services_btn');
+    const popups = document.querySelectorAll('.service_block_popup .popup');
 
-    document.addEventListener('click', function(event){
-        const clickedElement = event.target;
-        if(clickedButton === true){
-            popup
-        }
-    })
+    serviceButtons.forEach((button, index) => {
+        button.addEventListener('click', function () {
+            togglePopupVisibility(index);
+        });
+    });
 
-})
+    function togglePopupVisibility(index) {
+        popups.forEach((popup, i) => {
+            popup.style.display = i === index ? (popup.style.display === 'flex' ? 'none' : 'flex') : 'none';
+        });
+    }
+});
