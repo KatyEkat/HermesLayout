fb10e4d32517a9a7c18b65469fc7cf9f76d5bcbc
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-12-14 10:50:51 +0300

    clope-open popups

--- scripts/services.js
+++ scripts/services.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const serviceButtons = document.querySelectorAll('.services_btn');
     const popups = document.querySelectorAll('.service_block_popup .popup');
+    const popupBgrs = document.querySelectorAll('.service_block_popup .popup_bgr');
 
     serviceButtons.forEach((button, index) => {
         button.addEventListener('click', function () {
@@ -10,7 +11,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function togglePopupVisibility(index) {
         popups.forEach((popup, i) => {
-            popup.style.display = i === index ? (popup.style.display === 'flex' ? 'none' : 'flex') : 'none';
+            const displayStyle = i === index && popup.style.display !== 'flex' ? 'flex' : 'none';
+            popup.style.display = displayStyle;
+            popupBgrs[i].style.display = displayStyle;
         });
     }
+
+    document.addEventListener('click', function (event) {
+        const isClickInsidePopup = popups.some((popup) => popup.contains(event.target));
+        const isClickInsideButton = Array.from(serviceButtons).some((button) => button.contains(event.target));
+
+        if (!isClickInsidePopup && !isClickInsideButton) {
+            popups.forEach((popup, i) => {
+                popup.style.display = 'none';
+                popupBgrs[i].style.display = 'none';
+            });
+        }
+    });
 });
